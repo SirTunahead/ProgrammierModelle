aff35ce6252ebcfba8c09c96d8d9be9ef72f1adf
SirTunahead <ralf.speer@hs-furtwangen.de>
2020-11-24 17:20:15 +0100

    a bit of work done

--- Aufgabe4/CAufgaben/Aufgabe1.c
+++ Aufgabe4/CAufgaben/Aufgabe1.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
-
 int recursion(int g, int n){
-
-    if(n>1){
-        g = recursion(2*(g), n-1)+recursion(3*(g),n-1);
-    }else{
-        return g = n;
-    }
+    if(n>1) return g = recursion(2*(g), n-1)+recursion(3*(g),n-1);
+    return g;
 }
 
 int main(){
-    //g(n) = 2g(n-1)+3g(n-2), falls n >1 g(n)=n
-    //ab 40 n fängt es an zu dauern... ab 143.077 sek hab ich abgebrochen
-    // bei 30 n ~ 4.344 sek
-    int g;
-    int n = 40;
+    int g = 1;
+    int n = 20;
     printf("%d\n",recursion(g,n));
    return 0;
 }
