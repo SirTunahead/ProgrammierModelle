867f7cb3db844949d21a788bb04cab49c00a0cb8
speerral <ralf.speer@hs-furtwangen.de>
2020-12-02 00:56:15 +0100

    done

--- Aufgabe4/CAufgaben/Aufgabe1.c
+++ Aufgabe4/CAufgaben/Aufgabe1.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
 
 int recursion(int n){
-    if(n>1) return ((2*recursion(n-1))+3*(recursion(n-2)));
+    //2*g(n-1)+3*g(n-2) falls n>1; gn = n sonst
+    
+    if(n>1) return ((2*recursion(n-1))+(3*recursion(n-2))); 
     return n;
 }
 
 int iterative(int g, int n){
-    while(n>1){
-      
-      g = (2*g)+(3*g);
-      n -= 1.5;
-      /*  int oldG = g;
-        
-        g = 2*oldG;
-        int oldG2 = 3*g;
-        g = g+oldG2;
-        n -= 3;
-        */
+    
+    int g1 = 1;
+    int g2 = 0;
+
+    for(int i = 1; i<n; i++){
+        g = 2*g1+3*g2;
+        g2 = g1;
+        g1 = g;
     }
 
     return g;
 }
 
+int tail_help(int n, int g1,  int g2){
+     if(n>1) return ((2*tail_help(n-1, g1-1, 0))+(3*tail_help(n-2, 0, g2-2))); 
+     return n;
+}
+
 
 int tailrecursion(int n){
-    return iterative(1,n);
+    int g1 = n;
+    int g2 = n;
+    return tail_help(n,g1,g2);
 }
 
 int main(){
     
     int n = 5;
     printf("%d recusive\n",recursion(n));
-
-
     printf("%d iterative\n",iterative(1,n));
-
     printf("%d tailrec\n", tailrecursion(n));
    return 0;
 }
